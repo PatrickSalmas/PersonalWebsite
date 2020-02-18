91760daedef75aa2d9c3326a0973da9e30605272
Patrick Salmas <psalmas@ucsd.edu>
2020-02-18 08:16:21 -0800

    Added descriptions projects and experience page

--- public/footer.js
+++ public/footer.js
@@ -8,25 +8,13 @@ function placeFooter() {
 	document.getElementById('footer').style.removeProperty('display');
 }
 
-// function adjustFooterOld() {
-	// var body = document.body;
-	// var html = document.documentElement;
-	
-	// var height = Math.max( body.scrollHeight, body.offsetHeight, 
-                       // html.clientHeight, html.scrollHeight, html.offsetHeight );
-
-	// let footer = document.getElementById('footer');
-	// let footTop = (height-footerHeight).toString(10) + "px";
-	// console.log(height);
-	// console.log(footTop);
-	// footer.style = "top: " + footTop;
-// }
-
 function adjustFooter() {
-	// console.log(calcFooterPos());
-	let footer = document.getElementById('footer');
-	let footTop = (calcFooterPos() + footerHeight).toString(10)+"px";
-	footer.style = "top: " + footTop;
+	if(!onHomeScreen) {
+		console.log("footer adjust");
+		let footer = document.getElementById('footer');
+		let footTop = (calcFooterPos() + footerHeight).toString(10)+"px";
+		footer.style = "top: " + footTop;
+	}
 }
 
 function calcFooterPos() {
@@ -37,8 +25,6 @@ function calcFooterPos() {
 	
 	for(let i = 0; i < onScreenElems.length; i++) {
 		distanceDown.push(onScreenElems[i].offsetTop + onScreenElems[i].offsetHeight + navBarHeight);
-		// console.log(distanceDown[i]);
-		// console.log(onScreenElems[i]);
 	}
 	
 	return Math.max(...distanceDown);
