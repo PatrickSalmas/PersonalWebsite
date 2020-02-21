449ecf0b8859404f9054c0b2f097768bfcc72dd9
Patrick Salmas <psalmas@ucsd.edu>
2020-02-21 11:09:29 -0500

    Added mobile detection and formats proj & experience accordingly
    If mobile is detected, lines up all columns in projects & experience into single column

--- public/projects-experience.js
+++ public/projects-experience.js
@@ -2,19 +2,38 @@ var setWidth = 1125;
 var colWidth = 545;
 var gapWidth = 25;
 var increment = 30;
+var singleColumn = false;
 
 function adjustWidth() {
 	let width = window.innerWidth;
 	
 	if(width >= setWidth) {
+		if(!singleColumn) {
+			singleColumn = true;
+			reOrderCols();
+		}
 		let leftCol = document.getElementsByClassName('column-left');
 		let rightCol = document.getElementsByClassName('column-right');
 		
 		for(let i = 0; i < leftCol.length; i++) {
 			leftCol[i].style.marginLeft = ((width - ((colWidth*2) + gapWidth)) / 2).toString(10) + "px";
 		}
-		
+	} else {
+		if(singleColumn) {
+			restoreCols();
+		}
 	}
+	
+}
+
+// TODO: Function that reorders columns by "importance"
+function reOrderCols() {
+	
+}
+
+// TODO: Function that restores original column order
+function restoreCols() {
+	
 }
 
 window.addEventListener('resize', adjustWidth);
