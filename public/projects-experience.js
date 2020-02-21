52e4a03705ee87bb192466cf2592e06eaa47986c
Patrick Salmas <psalmas@ucsd.edu>
2020-02-21 09:45:58 -0500

    Added responsive design for projects & experience on wider screens

---
+++ public/projects-experience.js
@@ -0,0 +1,22 @@
+var setWidth = 1125;
+var colWidth = 545;
+var gapWidth = 25;
+var increment = 30;
+
+function adjustWidth() {
+	let width = window.innerWidth;
+	
+	if(width >= setWidth) {
+		let leftCol = document.getElementsByClassName('column-left');
+		let rightCol = document.getElementsByClassName('column-right');
+		
+		for(let i = 0; i < leftCol.length; i++) {
+			leftCol[i].style.marginLeft = ((width - ((colWidth*2) + gapWidth)) / 2).toString(10) + "px";
+			console.log(leftCol[i].style.marginLeft);
+		}
+		
+	}
+}
+
+
+window.addEventListener('resize', adjustWidth);
