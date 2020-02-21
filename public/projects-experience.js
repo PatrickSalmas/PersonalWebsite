f36d41f24adbe46f4a6e2f08a3a1ab34f17d661c
Patrick Salmas <psalmas@ucsd.edu>
2020-02-21 11:24:06 -0500

    fixed left-margin bug when going from "two-column stretch" to single column

--- public/projects-experience.js
+++ public/projects-experience.js
@@ -2,25 +2,30 @@ var setWidth = 1125;
 var colWidth = 545;
 var gapWidth = 25;
 var increment = 30;
+
+var singleColWidth = 800;
 var singleColumn = false;
 
 function adjustWidth() {
 	let width = window.innerWidth;
+	let leftCol = document.getElementsByClassName('column-left');
+	let rightCol = document.getElementsByClassName('column-right');
 	
 	if(width >= setWidth) {
-		if(!singleColumn) {
-			singleColumn = true;
-			reOrderCols();
+		if(singleColumn) {
+			singleColumn = false;
+			restoreCols();
 		}
-		let leftCol = document.getElementsByClassName('column-left');
-		let rightCol = document.getElementsByClassName('column-right');
 		
 		for(let i = 0; i < leftCol.length; i++) {
 			leftCol[i].style.marginLeft = ((width - ((colWidth*2) + gapWidth)) / 2).toString(10) + "px";
 		}
-	} else {
-		if(singleColumn) {
-			restoreCols();
+	} 
+	else if(width <= singleColWidth) {
+		if(!singleColumn) {
+			singleColumn = true;
+			reOrderCols();
+			leftCol[0].style = "";
 		}
 	}
 	
